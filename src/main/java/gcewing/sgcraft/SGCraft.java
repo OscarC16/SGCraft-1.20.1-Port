cf1d6e047da7f2f3add3a75cdb0db2cf95c04d34
OscarC16 <oscar.cendales@gmail.com>
2026-04-28 16:14:30 -0500

    Initialize clean NeoForge 1.21.11 project (v2.2.1)

--- src/main/java/gcewing/sgcraft/SGCraft.java
+++ src/main/java/gcewing/sgcraft/SGCraft.java
@@ -1,41 +1,11 @@
-package gcewing.sgcraft;
+﻿package gcewing.sgcraft;
 
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import gcewing.sgcraft.registry.ModBlocks;
-import gcewing.sgcraft.registry.ModItems;
-import gcewing.sgcraft.registry.ModCreativeTabs;
-import gcewing.sgcraft.registry.ModBlockEntities;
-import gcewing.sgcraft.registry.ModMenuTypes;
-import gcewing.sgcraft.registry.ModSounds;
-import gcewing.sgcraft.network.ModNetwork;
-import org.slf4j.Logger;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
 
-@Mod(SGCraft.MODID)
+@Mod("sgcraft")
 public class SGCraft {
-    public static final String MODID = "sgcraft";
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public SGCraft() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ModBlocks.BLOCKS.register(modEventBus);
-        ModItems.ITEMS.register(modEventBus);
-        ModCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
-        ModBlockEntities.BLOCK_ENTITIES.register(modEventBus);
-        ModMenuTypes.MENUS.register(modEventBus);
-        ModSounds.SOUND_EVENTS.register(modEventBus);
-
-        modEventBus.addListener(this::setup);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("SGCraft starting...");
-        event.enqueueWork(ModNetwork::register);
+    public SGCraft(IEventBus modEventBus) {
+        // Hello World
     }
 }
